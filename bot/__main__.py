65432f6c836810dd5ca61bddd9ab7d8dc43525eb
Claudio Ortiz <claudio.enoch@gmail.com>
2020-11-22 17:10:54 -0700

    comment line

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,6 +2,7 @@ from bot import bot, TOKEN
 from bot.cogs.basic import BasicCog
 from bot.cogs.dog import DogCog
 
+# Add cogs (blueprints) to bot and run
 bot.add_cog(BasicCog(bot))
 bot.add_cog(DogCog(bot))
 bot.run(TOKEN)
